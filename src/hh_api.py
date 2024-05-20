e4986966590b4c6c1df13756fc01391394090b29
helonely <idravil1211@gmail.com>
2024-05-20 17:46:25 +0500

    creat hh_api.py

---
+++ src/hh_api.py
@@ -0,0 +1,40 @@
+import requests
+from abc import ABC, abstractmethod
+
+
+class AbstractAPI(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def get_vacancies(self, keyword):
+        pass
+
+
+class HeadHunterAPI(AbstractAPI):
+    def __init__(self):
+        super().__init__()
+        self.url = 'https://api.hh.ru/vacancies'
+        self.headers = {'User-Agent': 'HH-User-Agent'}
+        self.params = {'text': '', 'page': 0, 'per_page': 100}
+        self.vacancies = []
+
+    def get_vacancies(self, keyword):
+        self.params['text'] = keyword
+        while self.params.get('page') != 20:
+            response = requests.get(self.url, headers=self.headers, params=self.params)
+            vacancies = response.json()['items']
+            self.vacancies.extend(vacancies)
+            self.params['page'] += 1
+        return self.vacancies
+
+
+# Создание экземпляра класса для работы с API сайтов с вакансиями
+hh_api = HeadHunterAPI()
+
+if __name__ == "__main__":
+    # Получение вакансий с hh.ru в формате JSON
+    hh_vacancies = hh_api.get_vacancies("Python")
+
+    for i in hh_vacancies:
+        print(i)
